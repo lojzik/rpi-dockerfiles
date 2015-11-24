5306b444d488e74d537f1f0fd4faae3f633740a1
lojzik <zxc@post.cz>
2015-11-24 22:05:04 +0100

    add example

---
+++ examples/www/index.php
@@ -0,0 +1,11 @@
+<html>
+  <body>
+    <ul>
+      <li><a href="phpinfo.php">phpinfo</a></li>
+      <li><a href="http://<?php echo $_SERVER['HTTP_HOST']?>:3000">gogs</a></li>
+      <li><a href="http://<?php echo $_SERVER['HTTP_HOST']?>:5000">asp.net test</a></li>
+      <li><a href="/tt-rss">tt-rss</a></li>
+      <li><a href="adminer-4.2.2.php">adminer</a></li>
+    </ul>
+  </body>
+</html>
